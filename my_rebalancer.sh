4bf7f85acc5bdade9bd12b9467037cac83575033
HODLmeTight <35168804+TrezorHannes@users.noreply.github.com>
2021-10-28 14:44:53 +0200

    Update my_rebalancer.sh

--- my_rebalancer.sh
+++ my_rebalancer.sh
@@ -47,7 +47,8 @@ do
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterF" ] || [ -z "$parameterD" ] || [ -z "$parameterJ" ]
+# if [ -z "$parameterF" ] || [ -z "$parameterD" ] || [ -z "$parameterJ" ]
+if [ -z "$parameterJ" ]
 then
    echo "Required parameters are empty";
    helpFunction
@@ -63,16 +64,19 @@ rebalance_something () {
 echo "python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --$feeoption $parameterF -$parameterD $1 -p $2"
 }
 
-# Parse parameters from the command initiation
-echo "Fee Attribute > $parameterF"
-echo "Direction Push (f) or Pull (t) > $parameterD"
-echo "Channel ID 1 > $parameterJ"
-echo "Channel ID 2 > $parameterK"
-echo "Channel ID 3 > $parameterL"
-echo "Channel ID 4 > $parameterM"
-echo "Channel ID 5 > $parameterN"
-echo "Channel ID 6 > $parameterO"
-echo "Channel ID 7 > $parameterP"
+# Direction-Selection Process
+   echo -e "\t You have successfully added one or more channels to be rebalanced. Now define where the liquidity should move to."
+   echo -e "\t - 👉 Pushing liquidity indicates low inbound, high amount of local outbound sats"
+   echo -e "\t - 👈 Pulling liquidity indicates high inbound, low amount of local outbound sats"
+   
+directions='Push(f) Pull (t)'
+
+PS3='Select Direction: '
+
+select direction in $directions
+do
+        echo "We'll go for --$direction"
+done 
 
 # Fee-Selection Process
    echo -e "\tfee-factor: set b/w 0.1 and 1.0. Compare the costs against the expected income, scaled by this factor. fee-factor 1.5, routes that cost at most 150% of the expected earnings"
@@ -85,11 +89,62 @@ PS3='Select Fee Option: '
 
 select feeoption in $feeoptions
 do
-        if [ $feeoption == 'Quit' ]
-        then
-                break
-        fi
+#        if [ $feeoption == 'Quit' ]
+#        then
+#                break
+#        fi
         echo "We'll go for --$feeoption"
+done 
+
+# Fee-Value Process
+   echo -e "\t Indicate the price-ranges you want to allow as ceiling for your rebalance across all channels indicated. This is not error prone, read carefully"
+   echo -e "\t - fee-factor:decimal set. Examples: 0.1 indicates rebalance for max 10% costs. 0.5 = 50%, 1.5 = 150%. Start low eg 0.5"
+   echo -e "\t - fee-limit: total amount of satoshis per rebalance, eg. 30"
+   echo -e "\t - fee-ppm-limit: fee-rate per million satoshis. Set to 200 will pay a max of 200 satoshis for every million satoshis rebalanced"
+   
+if [ $feeoption == 'fee-factor' ]
+then
+echo "Enter your fee-factor setting as decimal (recommended between 0.1 and 1.5)"
+read feevalue
+else
+	if [ $feeoption == 'fee-limit' ]
+	then
+	echo "Enter your fee-limit setting as total number of sats per rebalance (eg 50)"
+	read feevalue
+	else
+		if [ $feeoption == 'fee-ppm-limit' ]
+		then
+		echo "Enter your fee-ppm-limit (eg 200 for 200ppm)"
+		read feevalue
+		fi
+	fi
+fi
+echo "We'll go for --$feevalue"
+
+# Parse parameters from the command initiation
+echo "Fee Attribute > $feeoption"
+echo "Fee Value > $feevalue"
+echo "Direction Push (f) or Pull (t) > $direction"
+echo "Channel ID 1 > $parameterJ"
+echo "Channel ID 2 > $parameterK"
+echo "Channel ID 3 > $parameterL"
+echo "Channel ID 4 > $parameterM"
+echo "Channel ID 5 > $parameterN"
+echo "Channel ID 6 > $parameterO"
+echo "Channel ID 7 > $parameterP"
+
+kickoffs='Yes Cancel'
+
+PS3='Should we go ahead? '
+
+select kickoff in $kickoffs
+do
+#        if [ $kickoff == 'Cancel' ]
+#        then
+#                break
+#        fi
+        echo "All right let's f'ing go 🚀"
+done 
 
 #Channel 1
 echo "Starting the rebalancing on Channel 1"
@@ -149,5 +204,4 @@ else
                 fi
         fi
 fi
-done
 echo "ALL DONE - move on Pleb!"
