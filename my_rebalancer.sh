80fbc25c9cc65017f07627b22b70e86dd6d10a61
HODLmeTight <35168804+TrezorHannes@users.noreply.github.com>
2021-10-28 11:54:56 +0200

    Added fee-setting option and cleaned some code

--- my_rebalancer.sh
+++ my_rebalancer.sh
@@ -8,6 +8,34 @@ activate () {
   . ../.venv/bin/activate
 }
 
+# Set LN-Path for Rebalance-LND
+if [ "`uname -a | grep umbrel`" != "" ]
+then 
+	# LNPATH setting on umbrel
+	LNPATH="~/umbrel/lnd/"
+else
+	#Other installations
+# Set your own Path to LND in case the below does not work for you
+	LNPATH="~/.lnd/"
+fi
+
+# Fee-Selection Process
+   echo -e "\tFor fee-factor, set b/w 0.1 and 1.0. Compare the costs against the expected income, scaled by this factor. As an example, with --fee-factor 1.5, routes that cost at most 150% of the expected earnings are tried. Use values smaller than 1.0 to restrict routes to only consider those earning more/costing less. "
+   echo -e "\tFor fee-limit, enter total amount of Satoshis willing to spend per rebalance.  If set, only consider rebalance transactions that cost up to the given number of satoshis."
+   echo -e "\tIf set, only consider rebalance transactions that cost up to the given number of satoshis per 1M satoshis sent."
+   
+feeoptions='fee-factor fee-limit fee-ppm-limit Quit'
+
+HMT='Select Fee Option: '
+select feeoption in $feeoptions
+do
+	if [ $feeoption == 'Quit' ]
+	then
+		break
+	fi
+	echo "We'll go for --$feeoption"
+done
+
 helpFunction()
 {
    echo "FUCK - something went wrong"
@@ -43,6 +71,7 @@ then
 fi
 
 # Begin script in case all parameters are correct
+echo "Fee Setting is\t\t\t\t> $feeoption"
 echo "Fee Factor is\t\t\t\t> $parameterF"
 echo "Direction Push (f) or Pull (t) is\t> $parameterD"
 echo "Channel ID 1 is\t\t\t\t> $parameterJ"
@@ -55,79 +84,59 @@ echo "Channel ID 7 is\t\t\t\t> $parameterP"
 
 rebalance_something () {
 
-#Channel 1
-	echo "Starting the rebalancing with Ratio $1 on Channel 1"
+#	python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --fee-factor $parameterF -$parameterD $1 -p $2
+#	python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --fee-factor $parameterF -$parameterD $1 -p $3
+#	python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --fee-factor $parameterF -$parameterD $1 -p $4
+#	python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --fee-factor $parameterF -$parameterD $1 -p $5
+#	python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --fee-factor $parameterF -$parameterD $1
+echo "python ~/rebalance-lnd/rebalance.py --lnddir $LNPATH --$feeoption $parameterF -$parameterD $1 -p $2"
+}
 
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterJ -p $1
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterJ -p $2
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterJ -p $3
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterJ -p $4
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterJ
+#Channel 1
+echo "Starting the rebalancing with Ratio $1 on Channel 1"
+rebalance_something $parameterJ 10 30 50 70
+
 if [ -z "$parameterK" ] 
 then
    	echo "We are done for now with Channel 1 concluding, Pleb";
-
 else
 	echo "Starting the rebalancing with Ratio $1 on Channel 2"
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterK -p $1
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterK -p $2
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterK -p $3
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterK -p $4
-	python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterK
+	rebalance_something $parameterK 10 30 50 70
 
 	if [ -z "$parameterL" ]
 	then
         	echo "We are done for now with Channel 2 concluding, Pleb";
 	else 
         	echo "Starting the rebalancing with Ratio $1 on Channel 3"
-		python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterL -p $1
-		python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterL -p $2
-		python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterL -p $3
-		python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterL -p $4
-		python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterL
-
+		rebalance_something $parameterL 10 30 50 70
+		
 		if [ -z "$parameterM" ] 
 		then
 		        echo "We are done for now with Channel 3 concluding, Pleb";
 		else 
 		        echo "Starting the rebalancing with Ratio $1 on Channel 4"
-			python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterM -p $1
-			python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterM -p $2
-			python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterM -p $3
-			python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterM -p $4
-			python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterM
+			rebalance_something $parameterM 10 30 50 70
+			
 			if [ -z "$parameterN" ] 
 			then
 			        echo "We are done for now with Channel 4 concluding, Pleb";
 			else 
 			        echo "Starting the rebalancing with Ratio $1 on Channel 5"
-				python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterN -p $1
-				python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterN -p $2
-				python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterN -p $3
-				python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterN -p $4
-				python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterN
+				rebalance_something $parameterN 10 30 50 70
 
 				if [ -z "$parameterO" ] 
 				then
 				        echo "We are done for now with Channel 5 concluding, Pleb";
 				else 
 				        echo "Starting the rebalancing with Ratio $1 on Channel 6"
-					python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterO -p $1
-					python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterO -p $2
-					python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterO -p $3
-					python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterO -p $4
-					python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterO
-
+					rebalance_something $parameterO 10 30 50 70
+					
 					if [ -z "$parameterP" ] 
 					then
 					        echo "We are done for now with Channel 6 concluding, Pleb";
 					else 
 					        echo "Starting the rebalancing with Ratio $1 on Channel 7"
-						python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterP -p $1
-						python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterP -p $2
-						python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterP -p $3
-						python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterP -p $4
-						python ~/rebalance-lnd/rebalance.py --lnddir ~/umbrel/lnd/ --fee-factor $parameterF -$parameterD $parameterP
+						rebalance_something $parameterP 10 30 50 70
 
 					fi
 				fi
@@ -138,4 +147,3 @@ fi
 echo "ALL DONE - move on Pleb!"
 }
 
-rebalance_something 10 30 50 70
